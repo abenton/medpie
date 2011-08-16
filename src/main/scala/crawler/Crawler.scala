4639cffbd5d843ae956878de0e54b2c972002c29
Adrian Benton <adrian.donald.benton@gmail.com>
2011-08-16 16:38:50 -0400

    Added actor types and messages

--- src/main/scala/crawler/Crawler.scala
+++ src/main/scala/crawler/Crawler.scala
@@ -1,10 +1,60 @@
 package org.upenn.edu.medpie.crawler
 
-class Crawler(startUrl: String) {
+import scala.actors._
+
+sealed abstract class CrawlMsg
+
+// Write entry to log file
+case class WriteLog(time: java.util.Date, actor: Int, url: String, dst: String, msg: String) extends CrawlMsg
+
+// Scheduler -> Logger, called after finished crawling
+case class CloseLog extends CrawlMsg
+
+// Logger -> Scheduler, finished logging
+case class LogClosed extends CrawlMsg
+
+// Scheduler -> Crawler, save this page if matching and feed back more links
+case class CheckIt(url: String, waitMs: Int) extends CrawlMsg
+
+// Crawler -> Scheduler, links for next iteration
+case class Followed(links: List[String]) extends CrawlMsg
+
+/*
+ * Responsible for writing to the log file.
+ */
+class Logger(outPath: String) extends Actor {
   
+  def act() {
+    loop {
+      react {
+	case WriteLog(time, actor, url, dst, msg) => 
+      }
+    }
+  }
+  
+}
+
+/*
+ * Responsible for setting the crawlers on their merry ways.
+ */
+class Scheduler extends Actor {
+  
+}
+
+/*
+ * Dumps content of web pages to the directory of choice, based on
+ * the tasks given to it by the scheduler.
+ */
+class Crawler extends Actor {
   
 }
 
 object Crawler {
+  def crawl {
+    
+  }
+  
+  
+  
   
 }
